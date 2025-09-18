732a17fab585aa55bc89c57600cb129895a23538
Lakshmi-Sayyapureddy1819 <lakshmi.charvy1819@gmail.com>
2025-09-18 20:10:50 +0530

    updated

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def analyze_pdf_with_gemini(api_key, pdf_bytes, prompt):
         config=dict(mime_type="application/pdf")
     )
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-1.5-pro",
         contents=[uploaded, prompt]
     )
     return response.text
